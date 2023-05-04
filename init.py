b3ce1ad97b4a0768146cd5b7c596422eb46a404d
Ethan Denny <ethandenny@protonmail.com>
2023-05-04 23:36:39 +0000

    Add slash commands

--- init.py
+++ init.py
@@ -1,3 +1,4 @@
+from discord import app_commands
 from parse_query import parse_query
 
 import discord
@@ -5,40 +6,35 @@ import os
 
 
 client = discord.Client(intents=discord.Intents.all())
+tree = app_commands.CommandTree(client)
 
 
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
+    await tree.sync(guild=discord.Object(id=497544520695808000))
 
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
+@tree.command(name='ratemyprof', description='Rate profs!', guild=discord.Object(id=497544520695808000))
+async def ratemyprof(interaction, message: str):
+    query = message.replace(' ', '+')
 
-    if message.content.startswith('!ratemyprof '):
-        query = message.content[12:].replace(' ', '+')
+    firstName, lastName, avgRating, wouldTakeAgainPercent, avgDifficulty = parse_query(query)
 
-        firstName, lastName, avgRating, wouldTakeAgainPercent, avgDifficulty = parse_query(query)
+    if len(firstName) == 0:
+        await interaction.response.send_message('Sorry, I couldn\'t find that prof!')
+    else:
+        embed = discord.Embed(title=f'Professor Ratings For "{message}"')
 
-        if len(firstName) == 0:
-            await message.channel.send('Sorry, I couldn\'t find that prof!')
-        else:
-            embed = discord.Embed(title='Professor Ratings')
+        if len(firstName) > 1:
+            embed.description = 'Found multiple professors, here are the top results:'
+        
+        for i in range(len(firstName)):
+            percent = max(wouldTakeAgainPercent[i], 0)
+            value = f'Average rating is {avgRating[i]} and {percent:.0f}% would take again, with an average difficulty of {avgDifficulty[i]}'
+            embed.add_field(name=f'{firstName[i]} {lastName[i]}', value=value, inline=False)
 
-            if len(firstName) > 1:
-                embed.title = 'Professor Ratings'
-                embed.description = 'Found multiple professors, here are the top results:'
-            else:
-                embed.title = 'Professor Rating'
-            
-            for i in range(len(firstName)):
-                percent = max(wouldTakeAgainPercent[i], 0)
-                value = f'Average rating is {avgRating[i]} and {percent:.0f}% would take again, with an average difficulty of {avgDifficulty[i]}'
-                embed.add_field(name=f'{firstName[i]} {lastName[i]}', value=value, inline=False)
-
-            await message.channel.send(embed=embed)
+        await interaction.response.send_message(embed=embed)
 
 
 def main():
